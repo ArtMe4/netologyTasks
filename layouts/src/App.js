93c35d3550ee62909679e2f7497c045d8b1ef88d
Artur <artur133715@gmail.com>
2021-11-18 20:33:47 +0600

    feat: components logic

--- layouts/src/App.js
+++ layouts/src/App.js
@@ -1,25 +1,106 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function IconSwitch({icon, onSwitch}) {
+
+    return (
+        <span className="material-icons" onClick={onSwitch}>
+            {icon}
+        </span>
+    );
+
 }
 
-export default App;
+function CardsView({cards}) {
+
+    return (
+        cards.map((el, idx) => {
+            return (
+                <div key={idx}>
+                    card
+                    <ShopCard item={el} />
+                </div>
+            )
+        })
+    );
+
+}
+
+function ListView({cards}) {
+
+    return (
+        cards.map((el, idx) => {
+            return (
+                <div key={idx}>
+                    list
+                    <ShopCard item={el} />
+                </div>
+            )
+        })
+    );
+
+}
+
+function ShopCard({el}) {
+
+    return (
+        <div>1</div>
+    )
+
+}
+
+function Store() {
+
+    const products = [
+        {
+            name: "Nike Metcon 2",
+            price: "130",
+            color: "red",
+            img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/1.jpg"
+        },
+        {
+            name: "Nike Metcon 2",
+            price: "130",
+            color: "green",
+            img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/2.jpg"
+        },
+        {
+            name: "Nike Metcon 2",
+            price: "130",
+            color: "blue",
+            img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/3.jpg"
+        },
+        {
+            name: "Nike Metcon 2",
+            price: "130",
+            color: "black",
+            img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/4.jpg"
+        },
+        {
+            name: "Nike free run",
+            price: "170",
+            color: "black",
+            img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/7.jpg"
+        },
+        {
+            name: "Nike Metcon 3",
+            price: "150",
+            color: "green",
+            img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/layouts/img/5.jpg"
+        }
+    ];
+    const [list, setList] = useState(0)
+    const onSwitch = () => {
+        setList((list) => !list)
+    }
+
+    return (
+        <>
+            <IconSwitch icon={list ? 'view_list' : 'view_module'} onSwitch={onSwitch}/>
+            {list ? <ListView cards={products} /> : <CardsView cards={products} />}
+        </>
+    );
+}
+
+export default Store;
